1ca7ef7b7c4a4f097fb74dfeb8d2ef6e60b0c7e6
b0rey <b0rey.dj@gmail.com>
2020-01-04 11:01:29 +0300

    Create footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,6 +9,7 @@ import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
+import Footer from './footer'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -17,17 +18,11 @@ const Layout = ({ children }) => (
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
-        padding: `0px 1.0875rem 1.45rem`,
-        paddingTop: 0
+        maxWidth: 1000
       }}
     >
       <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
+      <Footer />
     </div>
   </>
 )
