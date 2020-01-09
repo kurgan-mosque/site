c06813760c8115012d0a33641a61f5c56bfe1453
b0rey <b0rey.dj@gmail.com>
2020-01-09 09:13:28 +0300

    Create sign component

---
+++ src/sign.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Sign = styled(props => (
+  <p {...props}>
+    <em>
+      С уважением и добрыми молитвами,
+      <br />
+      Председатель Курганского городского религиозного общества мусульман
+      <br />
+      Зиёдали Хаджи Мизробов.
+    </em>
+  </p>
+))`
+  text-align: right;
+`
+
+Sign.displayName = 'Sign'
+
+export default Sign
