4fcff713a6c231841489987d65b98642b48ce331
b0rey <b0rey.dj@gmail.com>
2020-01-09 20:18:51 +0300

    Create main and news page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
 
-const IndexPage = () => (
+import salaahTime from 'images/salaah-01-2020.jpg'
+
+const Index = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Главная" />
+    <article>
+      <img src={salaahTime} style={{ maxWidth: '100%' }} />
+    </article>
   </Layout>
 )
 
-export default IndexPage
+Index.displayName = 'Index'
+
+export default Index
