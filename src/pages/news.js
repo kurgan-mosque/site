e196071f07c4ea7127c69140aded56d3e93a1112
b0rey <b0rey.dj@gmail.com>
2020-01-09 20:18:51 +0300

    Create main and news page

---
+++ src/pages/news.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Layout from 'components/layout'
+import SEO from 'components/seo'
+
+import salaahTime from 'images/salaah-01-2020.jpg'
+
+const News = () => (
+  <Layout>
+    <SEO title="Новости" />
+    <h1>Новости</h1>
+    <article>
+      <img src={salaahTime} style={{ maxWidth: '100%' }} />
+    </article>
+  </Layout>
+)
+
+News.displayName = 'News'
+
+export default News
