7ae69881fd2d625c8848fbbd524561e26d38cd5a
b0rey <b0rey.dj@gmail.com>
2020-01-31 23:53:34 +0300

    layout/aside: Delete quran and links

--- src/layout/aside/aside.js
+++ src/layout/aside/aside.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import Quran from './quran'
 import Ayat from './ayat'
-import Links from './links'
 
 const Aside = styled(props => (
   <aside {...props}>
-    <Quran />
     <Ayat />
-    <Links />
   </aside>
 ))`
   width: 200px;
