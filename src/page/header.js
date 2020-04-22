dc36166b43be93a98feb56b61f059760c8c5a684
b0rey <b0rey.dj@gmail.com>
2020-04-22 23:54:22 +0300

    page: Create header

---
+++ src/page/header.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Header = styled.h1`
+  color: rgb(53, 53, 53);
+  text-transform: uppercase;
+  font-size: 21px;
+  margin: 2em 0 1em;
+  padding: 0 0.7em;
+  border-bottom: 1px solid #cee5dd;
+`
+
+Header.displayName = 'Header'
+
+export default Header
