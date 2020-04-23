b9ded8456163ccc968c3971b0f1b48e25eaa2af0
b0rey <b0rey.dj@gmail.com>
2020-04-23 23:59:31 +0300

    page: Create list of pages

---
+++ src/page/list.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Layout from 'layout'
+import SEO from 'seo'
+
+import PostList from './post/list'
+import Header from './header'
+
+export const PageList = ({ list, title }) => (
+  <Layout>
+    <SEO title={title} />
+    <Header>{title}</Header>
+    <PostList list={list} />
+  </Layout>
+)
+
+PageList.displayName = 'PageList'
+
+export default PageList
