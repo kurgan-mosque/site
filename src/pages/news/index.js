827d12bccddfd5e5fa03670eb0eff2721356db7b
b0rey <b0rey.dj@gmail.com>
2020-04-24 00:02:20 +0300

    pages/news: Add PageList

---
+++ src/pages/news/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import PageList from 'page/list'
+
+const PagesNews = ({ data }) => {
+  const list = data?.allMarkdownRemark?.nodes.map(
+    ({ frontmatter: { title }, fields: { slug }, excerpt }) => ({
+      title,
+      slug,
+      excerpt
+    })
+  )
+  return <PageList list={list} title="Новости" />
+}
+
+PagesNews.displayName = 'PagesNews'
+
+export default PagesNews
+
+export const pageQuery = graphql`
+  {
+    allMarkdownRemark(
+      filter: { fields: { slug: { regex: "^/news/" } } }
+      sort: { fields: fields___slug, order: DESC }
+    ) {
+      nodes {
+        frontmatter {
+          title
+        }
+        fields {
+          slug
+        }
+        excerpt
+      }
+    }
+  }
+`
