607ea72f9d115732c234bc97f970bc6b86b11a4d
b0rey <b0rey.dj@gmail.com>
2020-06-27 00:54:54 +0300

    layout/footer/social: Remove header

--- src/layout/footer/social.js
+++ src/layout/footer/social.js
@@ -20,43 +20,32 @@ const LINKS = [
 ]
 
 const Social = styled(props => (
-  <section {...props}>
-    <h3>Мы в социальных сетях:</h3>
-    <ul>
-      {LINKS.map(([link, Icon, color]) => (
-        <li key={link}>
-          <a href={link} target="_blank" rel="noopener noreferrer">
-            <Icon color={color} />
-          </a>
-        </li>
-      ))}
-    </ul>
-  </section>
+  <ul {...props}>
+    {LINKS.map(([link, Icon, color]) => (
+      <li key={link}>
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          <Icon color={color} />
+        </a>
+      </li>
+    ))}
+  </ul>
 ))`
   --height: 2em;
 
   text-align: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
 
-  h3 {
+  li {
     display: inline;
-    margin: 0;
-  }
+    margin-right: 1em;
 
-  ul {
-    list-style: none;
-    display: inline-block;
-    padding: 0;
-    margin: 0;
-
-    li {
-      display: inline;
-      margin-left: 1em;
-
-      svg {
-        height: var(--height);
-        width: var(--height);
-        vertical-align: middle;
-      }
+    svg {
+      height: var(--height);
+      width: var(--height);
+      vertical-align: middle;
     }
   }
 `
