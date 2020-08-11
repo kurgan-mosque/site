037a0f9d0e006fd3feeeef91994d947bfd79a88c
b0rey <b0rey.dj@gmail.com>
2020-08-11 22:44:04 +0300

    gatsby: combine source of files

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,11 +12,7 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
-      options: { name: `images`, path: `${__dirname}/src/images` },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: { name: `pages`, path: `${__dirname}/src/pages` },
+      options: { name: `images`, path: `${__dirname}/src` },
     },
     {
       resolve: `gatsby-plugin-manifest`,
